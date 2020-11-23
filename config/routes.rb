94e3553604872063306dc7572867030e59f6aae6
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-11-23 19:50:32 +0900

    Add route for block page and transaction page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'blocks#index'
+
+  resources :blocks, only: [:index, :show]
+  resources :transactions, only: [:index, :show]
 end
